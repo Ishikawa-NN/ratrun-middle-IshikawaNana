7e315eee332247c8eb544dfc3b51109f55e04dfa
Ishikawa-NN <59andu33@gmail.com>
2023-06-28 18:49:09 +0900

    設定完了

---
+++ ratrun-middle-IshikawaNana/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppHeader />
+    <NuxtPage />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import { AppHeader } from "../components/organisms/AppHeader";
+import { AppFooter } from "../components/organisms/AppFooter";
+export default {
+  components: { AppHeader, AppFooter },
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
